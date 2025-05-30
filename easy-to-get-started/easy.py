9a351563ab3deb3d01271f27d2943d93a55a246b
Ray <82130736+raystep-code@users.noreply.github.com>
2025-05-30 16:10:18 -0700

    Update easy.py
    added lic name
    
    Signed-off-by: Ray <82130736+raystep-code@users.noreply.github.com>

--- easy-to-get-started/easy.py
+++ easy-to-get-started/easy.py
@@ -1,5 +1,5 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
-
+# SPDX-License-Identifier: BSD-3-Clause
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the conditions in LICENSE.txt are met
 
